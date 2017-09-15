0efe9c843518ad2cda5c2e16745df4e5c63325ff
Lucas Sloan <lucas.sloan@gmail.com>
2017-09-15 15:13:52 -0700

    Bump the bazel version

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 mkdir tmp
 cd tmp
-curl --location --compressed https://github.com/bazelbuild/bazel/releases/download/0.5.2/bazel-0.5.2-installer-linux-x86_64.sh > bazel-0.5.2-installer-linux-x86_64.sh
+curl --location --compressed https://github.com/bazelbuild/bazel/releases/download/0.5.4/bazel-0.5.4-installer-linux-x86_64.sh > bazel-0.5.2-installer-linux-x86_64.sh
 chmod +x bazel-0.5.2-installer-linux-x86_64.sh
 ./bazel-0.5.2-installer-linux-x86_64.sh --user
 cd ..
