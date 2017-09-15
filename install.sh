aab60d618cfca0f23a2719c32427f04ee15440e9
Lucas Sloan <lucas.sloan@gmail.com>
2017-09-15 13:59:37 -0700

    Dont use travis-fold

--- install.sh
+++ install.sh
@@ -1,11 +1,7 @@
-travisFoldStart "bazel-install"
-  (
-    mkdir tmp
-    cd tmp
-    curl --location --compressed https://github.com/bazelbuild/bazel/releases/download/0.5.2/bazel-0.5.2-installer-linux-x86_64.sh > bazel-0.5.2-installer-linux-x86_64.sh
-    chmod +x bazel-0.5.2-installer-linux-x86_64.sh
-    ./bazel-0.5.2-installer-linux-x86_64.sh --user
-    cd ..
-    rm -rf tmp
-  )
-travisFoldEnd "bazel-install"
+mkdir tmp
+cd tmp
+curl --location --compressed https://github.com/bazelbuild/bazel/releases/download/0.5.2/bazel-0.5.2-installer-linux-x86_64.sh > bazel-0.5.2-installer-linux-x86_64.sh
+chmod +x bazel-0.5.2-installer-linux-x86_64.sh
+./bazel-0.5.2-installer-linux-x86_64.sh --user
+cd ..
+rm -rf tmp
